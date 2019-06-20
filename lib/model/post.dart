d98cbdd0941da3ffd04b83fb31fa159cfadf466f
zhangxu261 <zhangxu261@gmail.com>
2019-06-20 16:42:41 +0800

    update

---
+++ lib/model/post.dart
@@ -0,0 +1,89 @@
+class Post {
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl,
+  });
+
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+final List<Post> posts = [
+  Post(
+    title: 'Candy Shop',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3640162255,2305642911&fm=173&s=5F94CE056F4256530A9810C8030070FA&w=500&h=375&img.JPEG'
+  ),
+  Post(
+    title: 'Childhood in a picture',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2507121453,859666972&fm=173&s=83E8F205E653167756CCE68203005087&w=500&h=376&img.JPEG'
+  ),
+  Post(
+    title: 'Contained',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1884526206,3503749259&fm=173&s=C9D0C20962737F9678210BDF0300C0A5&w=500&h=375&img.JPEG'
+  ),
+  Post(
+    title: 'Dragon',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=604741417,2211288565&fm=173&s=1634C22565415F47149558D4030050B4&w=500&h=376&img.JPEG'
+  ),
+  Post(
+    title: 'Free Hugs',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3341871288,1160287870&fm=173&s=4AAA82454C032B4F5228ECAD03007003&w=500&h=375&img.JPEG'
+  ),
+  Post(
+    title: 'Gravity Falls',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=3024449846,2323825837&fm=173&s=F71A82294C026C45141914E203003030&w=500&h=376&img.JPEG'
+  ),
+  Post(
+    title: 'Icecream Truck',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3632325058,2052458363&fm=173&s=5A248C4446F36C3D5A161441030070FB&w=500&h=375&img.JPEG'
+  ),
+  Post(
+    title: 'keyclack',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=1326043567,3677358127&fm=173&s=7408BE552542D9474A83A56A03006039&w=500&h=374&img.JPEG'
+  ),
+  Post(
+    title: 'Overkill',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3267543139,518345399&fm=173&s=3BAAEB056F4252431431ECE30300D02F&w=500&h=376&img.JPEG'
+  ),
+  Post(
+    title: 'Say Hello to Barry',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=1979880221,3931757724&fm=173&s=16345A2545B2C067122664D9030040F9&w=500&h=375&img.JPEG'
+  ),
+  Post(
+    title: 'Space Skull',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=1790354464,3801251652&fm=173&s=CD1AA255021272615021F07F0300F068&w=500&h=376&img.JPEG'
+  ),
+  Post(
+    title: 'The Old Fashioned',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=2850679094,1975989839&fm=173&s=E7B1746C0EE5CF745CA26E950300E08F&w=500&h=376&img.JPEG'
+  ),
+  Post(
+    title: 'Tornado',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2240434455,605428895&fm=173&s=5E30A6442639322F5F9D4D3E0300802A&w=500&h=376&img.JPEG'
+  ),
+  Post(
+    title: 'Undo',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1307729259,1958441925&fm=173&s=7FB48F447A235F07E8347451030080EC&w=500&h=376&img.JPEG'
+  ),
+  Post(
+    title: 'White Dragon',
+    author: 'Mohamed Chahin',
+    imageUrl: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3507797269,2040247930&fm=173&s=D6997A237092DE634EA94EEE0300A026&w=500&h=377&img.JPEG'
+  )
+];
